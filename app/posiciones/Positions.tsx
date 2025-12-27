5798ca96b346a1a247dcb50e26b313f4aa5b8905
Diago Garcia <diagogh6@gmail.com>
2025-12-27 18:38:47 -0300

    mejora en la navegacion de posiciones

--- app/posiciones/Positions.tsx
+++ app/posiciones/Positions.tsx
@@ -275,7 +275,7 @@ export default function Posiciones() {
       </table>
 
       <button className="bg-blue-500 text-white px-4 py-2 rounded my-3">
-        <a href="../">Volver</a>
+        <a href={`../torneos/${torneoId}`}>Volver</a>
       </button>
     </div>
   );
